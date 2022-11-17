928b5c3b9f5f92d2b0b524a92ecd9295119a7090
Zachary Cairns <zcairns@email.unc.edu>
2022-11-17 12:48:01 -0500

    changed Console to console

--- cli.js
+++ cli.js
@@ -6,23 +6,19 @@ import minimist from 'minimist';
 const args = minimist(process.argv.slice(2));
 
 if(args.h){
-	Console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
     -e, -w        Longitude: E positive; W negative.
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.`)
+    -j            Echo pretty JSON from open-meteo API and exit.`);
 	process.exit(0);
 }
 
 const timezone = args.z || moment.tz.guess()
-console.log(args.n)
-console.log(args.w)
 let latitude = args.n || (-1 * args.s)
 let longtitude = args.e || (-1 * args.w)
-console.log(latitude)
-console.log(longtitude)
 let url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longtitude + "&daily=precipitation_hours&timezone=" + timezone;
 
 const response = await fetch(url)
